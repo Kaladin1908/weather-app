a51ee10b21fcd3eb0acd948ce9604b13513c4df2
Ishan Miglani <>
2022-11-22 01:37:49 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <title>Open Weather API Project</title>
+        <meta charSet='UTF-8'/>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="styles.css" />
+      </div>
+    <div>
+      <div className='weather-outer'>
+        <div id="loader" className='loader-container'><div className='loader'></div></div>
+        <div id="weather">
+          <p id="city"></p>
+          <p id="temp"></p>
+          <img id="icon" src="" />
+        </div>
+        <button id='getLocationButton'>Get My Location</button>
+      </div>
+      <script src="index.js"></script>
     </div>
+    </div>
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -10,8 +10,78 @@ root.render(
     <App />
   </React.StrictMode>
 );
+window.onload = function() {
+  let getLocationButton = document.getElementById("getLocationButton");
+console.log(getLocationButton);
+
+/**
+ * Adds functionality to location button.
+ * Retrieves current lat/long positions using geolocation.
+ */
+getLocationButton.addEventListener("click", () => {
+  // loader.style.display = 'block';
+  navigator.geolocation.getCurrentPosition((position) => {
+    let lat = position.coords.latitude;
+    let long = position.coords.longitude;
+
+    getWeatherData(lat, long);
+  });
+});
+
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+};
+
+var appid = "7ded7e19fcf114db598fcdfd64563b66";
+
+/**
+ * Fetches Open Weather Data API.
+ * @param {string} latitude - Current latitude.
+ * @param {string} longitude - Current longitude.
+ */
+async function getWeatherData(latitude, longitude) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appid}`,
+    requestOptions
+  );
+  var data = await response.json();
+  console.log(data); // Use this to explore the API response
+
+  displayData(data);
+  // loader.style.display = 'none';
+}
+
+/**
+ * Displays the weather data via HTML.
+ * @param {object} data - Open Weather Data API JSON response.
+ */
+function displayData(data) {
+  var cityName = data.name;
+  var icon = data.weather[0].icon;
+
+  document.getElementById("city").innerHTML = cityName;
+  document.getElementById("temp").innerHTML = convertToFahrenheit(
+    data.main.temp
+  );
+  document.getElementById(
+    "icon"
+  ).src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+}
+
+/**
+ * Converts Kelvin to Fahrenheit temperature scale.
+ * @param {string} temp - The title of the book.
+ * @returns {string} Current temperature in Fahrenheit.
+ */
+function convertToFahrenheit(temp) {
+  return Math.trunc((temp - 273.15) * 1.8 + 32) + "&deg; F";
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+}
