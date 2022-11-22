a47644c5126d903eecb6d986d43ee689aa251a10
Ishan Miglani <>
2022-11-22 11:17:56 -0500

    adding MapBox Support

--- src/App.js
+++ src/App.js
@@ -1,46 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 function App() {
-  return (
-    <div className='App'>
-      <div>
-        <title>Open Weather API Project</title>
-        <meta charSet='UTF-8'/>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="styles.css" />
-      </div>
-    <div>
-      <div className='weather-outer'>
-        <div id="loader" className='loader-container'><div className='loader'></div></div>
-        <div id="weather">
-          <p id="city"></p>
-          <p id="temp"></p>
-          <img id="icon" src="" />
-        </div>
-        <button id='getLocationButton'>Get My Location</button>
-      </div>
-      <script src="index.js"></script>
-    </div>
-    </div>
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+    setLng(map.current.getCenter().lng.toFixed(4));
+    setLat(map.current.getCenter().lat.toFixed(4));
+    setZoom(map.current.getZoom().toFixed(2));
+    });
+    });
     
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
   );
+  // return (
+  //   <div className='App'>
+  //     <div>
+  //       <title>Open Weather API Project</title>
+  //       <meta charSet='UTF-8'/>
+  //       <meta name="viewport" content="width=device-width, initial-scale=1" />
+  //       <link rel="stylesheet" href="styles.css" />
+  //     </div>
+  //   <div>
+  //     <div className='weather-outer'>
+  //       <div id="loader" className='loader-container'><div className='loader'></div></div>
+  //       <div id="weather">
+  //         <p id="city"></p>
+  //         <p id="temp"></p>
+  //         <img id="icon" src="" />
+  //       </div>
+  //       <button id='getLocationButton'>Get My Location</button>
+  //     </div>
+  //     <script src="index.js"></script>
+  //   </div>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,87 +1,87 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-window.onload = function() {
-  let getLocationButton = document.getElementById("getLocationButton");
-console.log(getLocationButton);
+// window.onload = function() {
+//   let getLocationButton = document.getElementById("getLocationButton");
+// console.log(getLocationButton);
 
-/**
- * Adds functionality to location button.
- * Retrieves current lat/long positions using geolocation.
- */
-getLocationButton.addEventListener("click", () => {
-  // loader.style.display = 'block';
-  navigator.geolocation.getCurrentPosition((position) => {
-    let lat = position.coords.latitude;
-    let long = position.coords.longitude;
+// /**
+//  * Adds functionality to location button.
+//  * Retrieves current lat/long positions using geolocation.
+//  */
+// getLocationButton.addEventListener("click", () => {
+//   // loader.style.display = 'block';
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     let lat = position.coords.latitude;
+//     let long = position.coords.longitude;
 
-    getWeatherData(lat, long);
-  });
-});
+//     getWeatherData(lat, long);
+//   });
+// });
 
-var requestOptions = {
-  method: "GET",
-  redirect: "follow",
-};
+// var requestOptions = {
+//   method: "GET",
+//   redirect: "follow",
+// };
 
-var appid = "7ded7e19fcf114db598fcdfd64563b66";
+// var appid = "7ded7e19fcf114db598fcdfd64563b66";
 
-/**
- * Fetches Open Weather Data API.
- * @param {string} latitude - Current latitude.
- * @param {string} longitude - Current longitude.
- */
-async function getWeatherData(latitude, longitude) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appid}`,
-    requestOptions
-  );
-  var data = await response.json();
-  console.log(data); // Use this to explore the API response
+// /**
+//  * Fetches Open Weather Data API.
+//  * @param {string} latitude - Current latitude.
+//  * @param {string} longitude - Current longitude.
+//  */
+// async function getWeatherData(latitude, longitude) {
+//   const response = await fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${appid}`,
+//     requestOptions
+//   );
+//   var data = await response.json();
+//   console.log(data); // Use this to explore the API response
 
-  displayData(data);
-  // loader.style.display = 'none';
-}
+//   displayData(data);
+//   // loader.style.display = 'none';
+// }
 
-/**
- * Displays the weather data via HTML.
- * @param {object} data - Open Weather Data API JSON response.
- */
-function displayData(data) {
-  var cityName = data.name;
-  var icon = data.weather[0].icon;
+// /**
+//  * Displays the weather data via HTML.
+//  * @param {object} data - Open Weather Data API JSON response.
+//  */
+// function displayData(data) {
+//   var cityName = data.name;
+//   var icon = data.weather[0].icon;
 
-  document.getElementById("city").innerHTML = cityName;
-  document.getElementById("temp").innerHTML = convertToFahrenheit(
-    data.main.temp
-  );
-  document.getElementById(
-    "icon"
-  ).src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-}
+//   document.getElementById("city").innerHTML = cityName;
+//   document.getElementById("temp").innerHTML = convertToFahrenheit(
+//     data.main.temp
+//   );
+//   document.getElementById(
+//     "icon"
+//   ).src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+// }
 
-/**
- * Converts Kelvin to Fahrenheit temperature scale.
- * @param {string} temp - The title of the book.
- * @returns {string} Current temperature in Fahrenheit.
- */
-function convertToFahrenheit(temp) {
-  return Math.trunc((temp - 273.15) * 1.8 + 32) + "&deg; F";
-}
+// /**
+//  * Converts Kelvin to Fahrenheit temperature scale.
+//  * @param {string} temp - The title of the book.
+//  * @returns {string} Current temperature in Fahrenheit.
+//  */
+// function convertToFahrenheit(temp) {
+//   return Math.trunc((temp - 273.15) * 1.8 + 32) + "&deg; F";
+// }
 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
 
-}
+// }
